2409fc4a0f5296cabf8155d4d9dcf5de4d549ca6
Matisslapisse <pokemathis1411@gmail.com>
2025-01-29 11:12:17 +0100

    UI Graph Price
    J'ai pu rajouter un affichage montrant le prix du graphique mais il ne s'affiche pas encore en jeu

--- Assets/Scripts/Graphic/GraphRenderer.cs
+++ Assets/Scripts/Graphic/GraphRenderer.cs
@@ -1,48 +1,55 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+using System.Collections.Generic; // Ajout de l'espace de noms pour List<T>
+using TMPro;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 public class GraphRenderer : MonoBehaviour
 {
     public LineRenderer lineRenderer;
-    
-    [Min(0)]
-    public int precision;
+    public GameObject textPrefab; // Prefab contenant un TextMeshPro
+    private TextMeshPro textComponent; // Un seul texte pour afficher la dernière valeur
+
+    [Min(0)] public int precision;
     public int maxStockValue;
-    
-    [Range(0, 10)]
-    public float height;
-    [Range(0, 10)]
-    public float width;
-    
-    
-    private List<float> stockValues;
-    
+
+    [Range(0, 10)] public float height;
+    [Range(0, 10)] public float width;
+
+    private List<float> stockValues; // Liste des valeurs du graphe
+
     private void Start()
     {
         stockValues = new List<float>();
+
         for (int i = 0; i < precision; i++)
+        {
             stockValues.Add(0);
+        }
+
+        // Création d'un seul texte pour afficher la dernière valeur
+        GameObject textObj = Instantiate(textPrefab, transform);
+        textComponent = textObj.GetComponent<TextMeshPro>();
+        textComponent.fontSize = 400; // Ajuste la taille du texte
+        textComponent.color = Color.white; // Couleur du texte
+        textComponent.text = "0"; // Valeur de départ
 
         DrawGraph();
     }
-    
+
     public void SetNewStockValue(int newValue)
     {
         stockValues.Add(newValue);
         stockValues.RemoveAt(0);
-        
+
         DrawGraph();
     }
-    
+
     private void DrawGraph()
     {
         lineRenderer.positionCount = precision;
         float currentPoint = 0;
+        Vector3 lastPosition = Vector3.zero;
 
-        foreach (float point in stockValues)
+        for (int i = 0; i < stockValues.Count; i++)
         {
             float minX = -width / 2;
             float maxX = width / 2;
@@ -51,12 +58,20 @@ public class GraphRenderer : MonoBehaviour
 
             float xPercent = currentPoint / precision;
             float x = Mathf.Lerp(minX, maxX, xPercent);
-            
-            float yPercent = point / maxStockValue;
+
+            float yPercent = stockValues[i] / maxStockValue;
             float y = Mathf.Lerp(minY, maxY, yPercent);
-            
-            lineRenderer.SetPosition((int)currentPoint, new Vector3(x, y , 0));
+
+            Vector3 position = new Vector3(x, y, 0);
+            lineRenderer.SetPosition(i, position);
+            lastPosition = position; // Stocke la dernière position
             currentPoint += 1;
         }
+
+        // Mise à jour du texte : 
+        textComponent.text = stockValues[stockValues.Count - 1].ToString(); // Affiche la dernière valeur
+
+        // Déplacer le texte avec un décalage (modifie ces valeurs pour voir les changements)
+        textComponent.transform.position = new Vector3(lastPosition.x + 430f, lastPosition.y + 210f, 0); // Ajuster la position selon les besoins
     }
 }
