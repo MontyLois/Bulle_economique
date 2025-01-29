ff968c3d67a893b9e41fd969ce1a44543a83366b
MontyLois <lois.monty@outlook.fr>
2025-01-29 11:20:48 +0100

    setting up some database

---
+++ Assets/Scripts/Core/GameController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static GameDatabase GameDatabase { get; private set; }
+    
+    //allow to load the class when the game start and before the scene load
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    private static void Load()
+    {
+        //generate the database
+        GameDatabase = new GameDatabase();
+    }
+}
---
+++ Assets/Scripts/Game/Bubble_Wand.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Bubble_WAnd
+{
+    //plus la valeur est haute, plus on peut accéder au bulle rare
+    private int rarity_max;
+
+    private void Create_Bubble()
+    {
+        
+    }
+}
